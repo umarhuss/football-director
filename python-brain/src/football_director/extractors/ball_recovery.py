062b956d4cbbc46d817d772210b6f0f51edbfac6
Umar Hussain <umar.huss25@gmail.com>
2026-07-16 17:42:27 +0100

    feat: add ball recovery metrics extractor

---
+++ python-brain/src/football_director/extractors/ball_recovery.py
@@ -0,0 +1,35 @@
+from .helper import update_avg
+
+def extract_ball_recovery_metrics(events:list)-> dict:
+    player_ball_recovery_metrics = {}
+
+    for event in events:
+        if event['type']['name'] == 'Ball Recovery':
+            p_id = event['player']['id']
+            loc_x = event['location'][0]
+            loc_y = event['location'][1]
+
+
+            if p_id not in player_ball_recovery_metrics:
+                ball_recovery_interface = {
+                    'id': p_id,
+                    'name': event['player']['name'],
+                    'total_recovery': 1,
+                    'avg_recovery_loc_x': loc_x,
+                    'avg_recovery_loc_y': loc_y
+                }
+
+                player_ball_recovery_metrics[p_id] = ball_recovery_interface
+
+            else:
+                curr_player = player_ball_recovery_metrics[p_id]
+                curr_x_avg = curr_player['avg_recovery_loc_x']
+                curr_y_avg = curr_player['avg_recovery_loc_y']
+                curr_total = curr_player['total_recovery']
+
+                curr_player['avg_recovery_loc_x'] = update_avg(curr_x_avg,curr_total,loc_x)
+                curr_player['avg_recovery_loc_y'] = update_avg(curr_y_avg,curr_total,loc_y)
+
+                curr_player['total_recovery'] += 1
+
+    return player_ball_recovery_metrics
