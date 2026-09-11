81dba970fbb931476e4fbadf0046ffaa9f2eee00
umarhuss <141486358+umarhuss@users.noreply.github.com>
2026-09-11 18:14:48 +0100

    Created the scaffolding for the library crate in rust where the extractors will live, renamed the namespace for the rust part of project to fd_engine (#1)

---
+++ fd_engine/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod ingestion{
+    pub mod ball_recovery{}
+    pub mod blocks{}
+    pub mod carries{}
+    pub mod clearances{}
+    pub mod dispossess{}
+    pub mod duels{}
+    pub mod fouls_conceded{}
+    pub mod fouls_won{}
+    pub mod interceptions{}
+    pub mod miscontrols{}
+    pub mod passes{}
+    pub mod pressures{}
+    pub mod shots{}
+    pub mod utils{}
+}
+
+
+
